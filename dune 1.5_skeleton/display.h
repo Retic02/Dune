3fede41c34edcee92d6397b8938da11515378b9f
최정훈 <162093630+Retic02@users.noreply.github.com>
2024-12-10 22:28:34 +0900

    Update display.h
    색상 추가

--- dune 1.5_skeleton/display.h
+++ dune 1.5_skeleton/display.h
@@ -1,8 +1,8 @@
 /*
 *  display.c:
-* ȭ�鿡 ���� ������ ���
-* ��, Ŀ��, �ý��� �޽���, ����â, �ڿ� ���� ���
-* io.c�� �ִ� �Լ����� �����
+* 화면에 게임 정보를 출력
+* 맵, 커서, 시스템 메시지, 정보창, 자원 상태 등등
+* io.c에 있는 함수들을 사용함
 */
 
 #ifndef _DISPLAY_H_
@@ -10,17 +10,29 @@
 
 #include "common.h"
 
-// ǥ���� ���� ����. ���� ���� �״µ�, ���ⲯ �߰��ϰų� �����ϱ�
-#define COLOR_DEFAULT	15
-#define COLOR_CURSOR	112
-#define COLOR_RESOURCE  112
+// 표시할 색상 정의. 대충 맞춰 뒀는데, 취향껏 추가하거나 변경하기
+#define COLOR_DEFAULT   (BACKGROUND_BLUE | BACKGROUND_GREEN | BACKGROUND_RED | BACKGROUND_INTENSITY)
+#define COLOR_CURSOR    (BACKGROUND_INTENSITY | BACKGROUND_BLUE)
+#define COLOR_RESOURCE  (BACKGROUND_INTENSITY | BACKGROUND_BLUE)
+#define COLOR_ATREIDES  (BACKGROUND_BLUE | BACKGROUND_INTENSITY)
+#define COLOR_HARKONNEN (BACKGROUND_RED | BACKGROUND_INTENSITY)
+#define COLOR_SANDWORM  (BACKGROUND_RED | BACKGROUND_GREEN)
+#define COLOR_PLATE     (0)
+#define COLOR_SPICE     (BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_INTENSITY)
+#define COLOR_ROCK      (BACKGROUND_INTENSITY)
+#define COLOR_EMPTY     (BACKGROUND_BLUE | BACKGROUND_GREEN | BACKGROUND_RED) // 빈 공간의 기본 배경색
 
-// ������ �ڿ�, ��, Ŀ���� ǥ��
-// ������ ȭ�鿡 ǥ���� ����� ���⿡ �߰��ϱ�
+
+// 지금은 자원, 맵, 커서만 표시
+// 앞으로 화면에 표시할 내용들 여기에 추가하기
 void display(
 	RESOURCE resource,
 	char map[N_LAYER][MAP_HEIGHT][MAP_WIDTH],
 	CURSOR cursor
 );
 
+void display_status();
+void display_command();
+void update_screen();
+void setColor(HANDLE hConsole, int color);
 #endif
